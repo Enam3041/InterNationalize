4472fb6b87cea86944e9e43410b5aad1e4aeaa13
Enamul <enamul.3041@gmail.com>
2020-09-03 10:11:25 +0600

    Initial Commit

---
+++ Internationalize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Internationalize
+//
+//  Created by Enam on 9/3/20.
+//  Copyright © 2020 Enam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
